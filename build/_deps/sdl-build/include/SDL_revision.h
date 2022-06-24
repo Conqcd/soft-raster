c47c2b5ed3148db1eeaa9d18e44df5991055cdbc
Conqcd <411277487@qq.com>
2022-06-24 13:29:22 +0800

    andos

--- build/_deps/sdl-build/include/SDL_revision.h
+++
@@ -1,6 +0,0 @@
-#define SDL_REVISION "https://github.com/Conqcd/soft-raster.git@d8df9305daa4928763e835e2ce74452172cde938"
-#define SDL_REVISION_NUMBER 0
-
-#ifndef SDL_REVISION
-#define SDL_REVISION ""
-#endif
